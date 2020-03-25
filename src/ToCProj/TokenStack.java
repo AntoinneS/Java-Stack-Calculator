fc7815d59c2164053ad122009b42a0aed361fd16
Antoinne <antoinnestewart@yahoo.com>
2020-03-24 23:21:48 -0500

    Commit and Push

---
+++ src/ToCProj/TokenStack.java
@@ -0,0 +1,30 @@
+package ToCProj;
+import java.util.*; 
+import java.io.*;
+import java.lang.*;
+public class TokenStack
+{
+  /** Member variables **/
+  private ArrayList<Token> tokens;
+
+  /** Constructors **/
+  public TokenStack() {
+    tokens = new ArrayList<Token>();
+  }
+
+  /** Accessor methods **/
+  public boolean isEmpty() {
+    return tokens.size() == 0;
+  }
+  public Token top() {
+    return tokens.get(tokens.size()-1);
+  }
+
+  /** Mutator methods **/
+  public void push(Token t) {
+    tokens.add(t);
+  }
+  public void pop() {
+    tokens.remove(tokens.size()-1);
+  }
+}
